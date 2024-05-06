661eccc99b52de1024b819fbe5f9bbee1c6f5482
Ajit Kumar Chaudhary <chaudharyajit7172@gmail.com>
2024-05-06 16:16:51 +0545

    index code modified

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+router.get("/", (req, res) => {
+    res.json({msg:"hello ajit"})
+}); 
 
 router.post("/register", (req, res, next) => {
     try{
@@ -10,18 +13,19 @@ router.post("/register", (req, res, next) => {
     });
 
 // login scenario
-router.post("/", (req, res, next) => {
-    try {
+router.post("/login", (req, res, next) => {
+    try{
     const { email, password} = req.body;
     console.log({ email, password});
     //logic
    if (email !== "chaudharyajit7172@gmail.com" || password !== "123") {
     throw new Error("Invalid Credentials");
-   } 
+   } else {
 res.json({ msg:"login user"});
-    } catch (error) {
-       next(error);
     }
+} catch (error) {
+    next(error);
+}
 });
 
 module.exports = router;
